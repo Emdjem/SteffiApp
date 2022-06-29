0d5af8f6446073294bc6da49804d9df1308dd11d
Emdjem <64790976+Emdjem@users.noreply.github.com>
2022-06-29 13:24:39 +0200

    test

---
+++ script.js
@@ -0,0 +1,44 @@
+document.querySelector('#push').onclick = function ()
+{
+    if(document.querySelector('#newtask input').value.length == 0)
+    {
+        alert("Stefi, please enter a task")
+    }
+    else
+    {
+        document.querySelector('#tasks').innerHTML 
+        += `  
+        <div class="task">
+          <span id="taskname">
+            ${document.querySelector('#newtask input').value}
+          </span>
+      
+      <button class="delete">
+      <i class="material-symbols-outlined">
+        delete
+      </i>
+      </button>
+      </div>
+      `;
+
+      var current_task = document.querySelectorAll(".delete");
+      for(var i = 0; i < current_task.length; i++)
+      {
+          current_task[i].onclick = function()
+          {
+              this.parentNode.remove();
+          }
+      }
+
+      var tasks = document.querySelectorAll(".task");
+      for(var i = 0; i < tasks.length; i++)
+      {
+          tasks[i].onclick = function()
+          {
+              this.classList.toggle('done');
+          }
+        }
+
+    document.querySelector("#newtask input").value = "";
+
+}}
